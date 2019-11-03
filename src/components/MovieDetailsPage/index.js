feb7bb3741c146066fd174a8e846835c1ac4c46a
Tom Duggan <tduggan@squarespace.com>
2019-11-03 12:15:25 +0200

    update comment

--- src/components/MovieDetailsPage/index.js
+++ src/components/MovieDetailsPage/index.js
@@ -4,9 +4,16 @@ import { Link } from 'react-router-dom'
 
 const MOVIE_POSTER_URL_PREFIX = 'https://image.tmdb.org/t/p/w780' /* Prepend this to the the movie poster_path or backdrop_path to get a full image src */
 
-const MovieDetailsPage = () => {
+const MovieDetailsPage = ( props ) => {
   
-  /* TODO: Connect to redux and display movie details based on this.props.match.params.movieIndex */
+  /* TODO: Connect to redux and display movie details based on 
+  props.match.params.movieIndex
+  
+  you can display:
+  movie.backdrop_path or movie.poster_path,
+  movie.original_title,
+  movie.overview
+  */
 
   return (
     <div className='movie-details'>
